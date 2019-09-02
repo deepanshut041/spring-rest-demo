c78e67567c8d3a55ce2d7d023db4d5d8d12f1b64
deepanshut041 <deepanshut041@gmail.com>
2019-09-02 20:49:12 +0530

    Initail Commit

---
+++ src/main/java/io/github/deepanshut041/serendeepia/SerendeepiaApplication.java
@@ -0,0 +1,13 @@
+package io.github.deepanshut041.serendeepia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerendeepiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerendeepiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/deepanshut041/serendeepia/SerendeepiaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.deepanshut041.serendeepia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerendeepiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
